542ca30be9a769cea986cc32a1f8d75b16b31eb3
Jata <jataellen@yahoo.ca>
2025-04-17 12:39:22 -0300

    cgi exp section + upload as docx

---
+++ utils/document_utils.py
@@ -0,0 +1,39 @@
+from spire.doc import *
+from spire.doc.common import *
+
+
+# Function to convert a single DOCX file to PDF using Pandoc
+def convert_docx_to_pdf(uploaded_file, temp_file_path, pdf_file_path):
+
+    # Save the uploaded DOCX file temporarily
+    with open(temp_file_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+
+    print(f"File saved as: {temp_file_path}")
+
+    document = Document()
+    document.LoadFromFile(temp_file_path)
+
+    document.SaveToFile(pdf_file_path, FileFormat.PDF)
+    document.Close()
+
+    return pdf_file_path
+
+
+def convert_to_pdf(uploaded_file, file_id):
+
+    temp_file_path = f"temp_{file_id}.docx"
+    pdf_file_path = f"temp_{file_id}.pdf"
+
+    if (
+        uploaded_file.type
+        == "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+    ):
+        temp_file_path = convert_docx_to_pdf(
+            uploaded_file, temp_file_path, pdf_file_path
+        )
+
+    elif uploaded_file.type == "application/pdf":  # PDF MIME type
+        with open(pdf_file_path, "wb") as f:
+            f.write(uploaded_file.read())
+    return temp_file_path
